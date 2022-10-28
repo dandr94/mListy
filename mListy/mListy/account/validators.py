e6241e72c63daefd62c153cc390bf831033bee59
dandr94 <d.andreev1994@gmail.com>
2022-10-28 20:42:00 +0300

    Added account app and utils for it

---
+++ mListy/mListy/account/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+
+USERNAME_ERROR_MESSAGE = 'Username can contains only letters and numbers.'
+NAME_ERROR_MESSAGE = 'Name can only contains letters.'
+
+
+def validate_only_letters_and_numbers(value):
+    if not value.isalnum():
+        raise ValidationError(USERNAME_ERROR_MESSAGE)
+
+
+def validate_only_letters(value):
+    if not value.isalpha():
+        raise ValidationError(NAME_ERROR_MESSAGE)
