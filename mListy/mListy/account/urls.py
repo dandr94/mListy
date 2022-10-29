9987d17aefc6afc92a0a5e6e53c4c498f0b6b5b1
dandr94 <d.andreev1994@gmail.com>
2022-10-29 19:36:47 +0300

    Update urls.py for account

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', HomeViewNoProfile.as_view(), name='index'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('logout/', LogoutUserView.as_view(), name='logout user')
+    path('logout/', LogoutUserView.as_view(), name='logout')
 ]
