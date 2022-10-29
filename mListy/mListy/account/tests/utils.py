5214a180fb4083981daee4e7131ae5b47f095618
dandr94 <d.andreev1994@gmail.com>
2022-10-29 21:55:39 +0300

    Added utils for tests

---
+++ mListy/mListy/account/tests/utils.py
@@ -0,0 +1,10 @@
+VALID_USER_CREDENTIALS = {
+    'username': 'foobar',
+    'email': 'foo@bar.barz',
+    'password': 'foo123barz'
+}
+
+VALID_LOGIN_CREDENTIALS = {
+    'username': 'foobar',
+    'password': 'foo123barz'
+}
