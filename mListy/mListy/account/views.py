8c8966ed9d43c174fe8c1ab395070d399d7beca2
dandr94 <d.andreev1994@gmail.com>
2022-10-29 17:12:25 +0300

    Added authentication views

--- mListy/mListy/account/views.py
+++ mListy/mListy/account/views.py
@@ -1,10 +1,33 @@
-from django.views.generic import TemplateView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from mListy.account.forms import CreateUserForm, LoginAccountForm
 
 
 class HomeViewNoProfile(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user  # FIX NOT NECESSARY
-        return context
+
+class RegisterUserView(CreateView):
+    form_class = CreateUserForm
+    template_name = 'account/register.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, *args, **kwargs):
+        result = super().form_valid(*args, **kwargs)
+        login(self.request, self.object)
+        return result
+
+
+class LoginUserView(LoginView):
+    form_class = LoginAccountForm
+    template_name = 'account/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class LogoutUserView(LogoutView):
+    pass
