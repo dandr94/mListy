66319b71c39cacf44fe3f3cf2b25086980e2e4f5
dandr94 <d.andreev1994@gmail.com>
2022-10-29 17:12:03 +0300

    Added index, register, login, logout urls

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from mListy.account.views import HomeViewNoProfile
+from mListy.account.views import HomeViewNoProfile, RegisterUserView, LoginUserView, LogoutUserView
 
 urlpatterns = [
-    path('', HomeViewNoProfile.as_view(), name='index')
+    path('', HomeViewNoProfile.as_view(), name='index'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', LogoutUserView.as_view(), name='logout user')
 ]
