ee4f88750d870ec80499f7c4ade9ab25239f8eb0
dandr94 <d.andreev1994@gmail.com>
2022-10-29 19:35:23 +0300

    Update forms for create user

--- mListy/mListy/account/forms.py
+++ mListy/mListy/account/forms.py
@@ -1,33 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.validators import MinLengthValidator
-from django.db import IntegrityError
 
 from mListy.account.models import Profile
-from mListy.account.validators import validate_only_letters_and_numbers
 
 UserModel = get_user_model()
 
 
 class CreateUserForm(UserCreationForm):
-    username = forms.CharField(
-        max_length=Profile.USERNAME_MAX_CHAR,
-        validators=[MinLengthValidator(Profile.USERNAME_MIN_CHAR),
-                    validate_only_letters_and_numbers]
-    )
+    FIELD_NAME_HELP_TEXT_TO_IGNORE = ['username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name in ['username', 'email', 'password1', 'password2']:
+        for field_name in self.FIELD_NAME_HELP_TEXT_TO_IGNORE:
             self.fields[field_name].help_text = None
 
     def save(self, commit=True):
         user = super().save(commit=commit)
 
         profile = Profile(
-            username=self.cleaned_data['username'],
             user=user
         )
 
