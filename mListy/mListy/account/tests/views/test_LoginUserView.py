fa35c42f98225161b89da67c0e08faffac25d4d5
dandr94 <d.andreev1994@gmail.com>
2022-10-29 21:55:14 +0300

    Added tests for login view

---
+++ mListy/mListy/account/tests/views/test_LoginUserView.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, RequestFactory
+from mListy.account.tests.utils import VALID_LOGIN_CREDENTIALS, VALID_USER_CREDENTIALS
+
+UserModel = get_user_model()
+
+
+class LoginUserViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = UserModel.objects.create_user(
+            **VALID_USER_CREDENTIALS)
+
+    def test_login_with_valid_credentials(self):
+        response = self.client.post('/login/', VALID_LOGIN_CREDENTIALS, follow=True)
+        self.assertTrue(response.context['user'].is_active)
---
+++ mListy/mListy/account/tests/views/test_RegisterUserView.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+UserModel = get_user_model()
+
+
+class RegisterUserViewTests(TestCase):
+    pass
