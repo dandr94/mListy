c2aa2bffd3f2aa6152f83924ae740b0d9c125ff6
dandr94 <d.andreev1994@gmail.com>
2022-10-30 18:57:00 +0200

    Update test for LoginUserView

--- mListy/mListy/account/tests/views/test_LoginUserView.py
+++ mListy/mListy/account/tests/views/test_LoginUserView.py
@@ -6,10 +6,12 @@ UserModel = get_user_model()
 
 
 class LoginUserViewTests(TestCase):
+    PATH = '/login/'
+
     def setUp(self):
         self.user = UserModel.objects.create_user(
             **VALID_USER_CREDENTIALS)
 
     def test_login_with_valid_credentials(self):
-        response = self.client.post('/login/', VALID_LOGIN_CREDENTIALS, follow=True)
+        response = self.client.post(self.PATH, VALID_LOGIN_CREDENTIALS, follow=True)
         self.assertTrue(response.context['user'].is_active)
