29c819dc5e53174d57a1e2e30e2297249ca66fa8
dandr94 <d.andreev1994@gmail.com>
2022-10-31 18:02:25 +0200

    Registered account models in administration

--- mListy/mListy/account/admin.py
+++ mListy/mListy/account/admin.py
@@ -1,2 +1,16 @@
 from django.contrib import admin
 
+from mListy.account.models import Profile, mListyUser
+
+admin.site.site_header = 'mListy Administration'
+
+
+@admin.register(mListyUser)
+class mListyUser(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_staff', 'is_superuser', ]
+
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'facebook', 'instagram', 'twitter', 'website']
+
