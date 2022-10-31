104bf167a8a8f606201d35ed9e63ba9f600b2d6e
dandr94 <d.andreev1994@gmail.com>
2022-10-31 21:33:00 +0200

    Update urls.py for account

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from mListy.account.views import HomeViewNoProfile, RegisterUserView, LoginUserView, LogoutUserView, ProfileDetailsView
+from mListy.account.views.auth import RegisterUserView, LoginUserView, LogoutUserView
+from mListy.account.views.generic import HomeViewNoProfile, Dashboard
+from mListy.account.views.profile import ProfileDetailsView
 
 urlpatterns = [
     path('', HomeViewNoProfile.as_view(), name='index'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
