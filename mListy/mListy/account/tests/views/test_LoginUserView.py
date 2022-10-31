4c045182d5e17094d883de9144e00d0a88f103aa
dandr94 <d.andreev1994@gmail.com>
2022-10-31 20:46:21 +0200

    Fix test for LoginUserView

--- mListy/mListy/account/tests/views/test_LoginUserView.py
+++ mListy/mListy/account/tests/views/test_LoginUserView.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+
+from mListy.account.models import Profile
 from mListy.account.tests.utils import VALID_LOGIN_CREDENTIALS, VALID_USER_CREDENTIALS
 
 UserModel = get_user_model()
@@ -7,11 +9,15 @@ UserModel = get_user_model()
 
 class LoginUserViewTests(TestCase):
     PATH = '/login/'
+    REDIRECT_TEMPLATE = 'index.html'
 
     def setUp(self):
         self.user = UserModel.objects.create_user(
             **VALID_USER_CREDENTIALS)
+        self.profile = Profile(user=self.user)
+        self.profile.save()
 
     def test_login_with_valid_credentials(self):
         response = self.client.post(self.PATH, VALID_LOGIN_CREDENTIALS, follow=True)
-        self.assertTrue(response.context['user'].is_active)
+        self.assertTemplateUsed(response, self.REDIRECT_TEMPLATE)
+        self.assertTrue(response.context['user'].is_authenticated)
