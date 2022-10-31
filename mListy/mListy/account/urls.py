b4d005fad74b006bf7e28231bbfb8fd19db7de89
dandr94 <d.andreev1994@gmail.com>
2022-10-31 18:03:45 +0200

    Added slug url for proifle details

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from mListy.account.views import HomeViewNoProfile, RegisterUserView, LoginUserView, LogoutUserView
+from mListy.account.views import HomeViewNoProfile, RegisterUserView, LoginUserView, LogoutUserView, ProfileDetailsView
 
 urlpatterns = [
     path('', HomeViewNoProfile.as_view(), name='index'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('logout/', LogoutUserView.as_view(), name='logout')
+    path('logout/', LogoutUserView.as_view(), name='logout'),
+
+    path('profile/<str:slug>/', ProfileDetailsView.as_view(), name='profile details')
 ]
