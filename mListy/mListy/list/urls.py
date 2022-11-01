bc623925b2bfe7397d404ca714522a322b9a61ee
dandr94 <d.andreev1994@gmail.com>
2022-11-01 20:03:13 +0200

    Added DeleteListView in urls

--- mListy/mListy/list/urls.py
+++ mListy/mListy/list/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from mListy.list.views import CreateListView, EditListView
+from mListy.list.views import CreateListView, EditListView, DeleteListView
 
 urlpatterns = [
     path('create_list/', CreateListView.as_view(), name='create list'),
-    path('list/edit/<str:slug>/', EditListView.as_view(), name='edit list')
+    path('list/edit/<str:slug>/', EditListView.as_view(), name='edit list'),
+    path('list/delete/<str:slug>/', DeleteListView.as_view(), name='delete list')
 ]
