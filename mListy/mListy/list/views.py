b731d5336e827b7e8526fc7ed322ff10f28d5f03
dandr94 <d.andreev1994@gmail.com>
2022-11-01 16:21:37 +0200

    Added CreateListView

--- mListy/mListy/list/views.py
+++ mListy/mListy/list/views.py
@@ -1,2 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from mListy.list.forms import CreateListForm
+
+
+class CreateListView(LoginRequiredMixin, CreateView):
+    form_class = CreateListForm
+    template_name = 'list/create_list.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
