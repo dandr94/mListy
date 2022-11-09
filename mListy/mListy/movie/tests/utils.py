a85ca686dbf37f8984d4a24de9a1489bd5d1999f
dandr94 <d.andreev1994@gmail.com>
2022-11-09 19:00:07 +0200

    Added utils.py

---
+++ mListy/mListy/movie/tests/utils.py
@@ -0,0 +1,17 @@
+VALID_MOVIEDB_DATA = {
+    'movie_id': 54212,
+    'name': 'FooBarBarz',
+    'poster': 'https://foo.bar',
+    'description': 'Lorem ipsum un mass foo bar.',
+    'duration': '155',
+    'genres': 'Action, Drama',
+    'average_grade': 9.5,
+    'actors': 'Foo, Bar, Barz',
+    'roles': 'Barz, Bar, Foo',
+    'production_companies': 'FromBarzSoftware',
+    'language': 'En',
+    'imdb_link': 'https://foo.bar',
+    'budget': '1500000',
+    'release_date': '2022-12-08',
+    'status': 'Released'
+}
