98ba54afc8b2f5607ea009dcf75e319c8d4f38f9
dandr94 <d.andreev1994@gmail.com>
2022-11-09 17:27:21 +0200

    Added movie ulrs to global urls

--- mListy/mListy/urls.py
+++ mListy/mListy/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mListy.account.urls')),
-    path('', include('mListy.list.urls'))
+    path('', include('mListy.list.urls')),
+    path('', include('mListy.movie.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
