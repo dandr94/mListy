3ed789551b9468166fe733c40933c744a9eec1d2
dandr94 <d.andreev1994@gmail.com>
2022-11-09 17:43:55 +0200

    Register MovieDB in administration

--- mListy/mListy/movie/admin.py
+++ mListy/mListy/movie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from mListy.movie.models import MovieDB
+
+
+@admin.register(MovieDB)
+class MovieDBAdmin(admin.ModelAdmin):
+    list_display = ['movie_id', 'name', 'poster', 'description', 'duration', 'genres', 'average_grade', 'actors',
+                    'roles', 'production_companies', 'language', 'imdb_link', 'budget', 'release_date', 'status']
