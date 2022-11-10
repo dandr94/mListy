ac9254fdebf15a3d10e03135a51846abc9f8c221
dandr94 <d.andreev1994@gmail.com>
2022-11-10 19:31:30 +0200

    Add slugfield for moviedb

--- mListy/mListy/movie/models.py
+++ mListy/mListy/movie/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
+
+UserModel = get_user_model()
 
 
 class MovieDB(models.Model):
@@ -77,6 +81,14 @@ class MovieDB(models.Model):
     last_updated = models.DateField(
         auto_now=True
     )
+
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
