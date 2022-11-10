2744049f77f38006012dfb1bf34b06c11d7c6007
dandr94 <d.andreev1994@gmail.com>
2022-11-10 19:29:23 +0200

    Register ListEntry

--- mListy/mListy/list/admin.py
+++ mListy/mListy/list/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from mListy.list.models import List
+from mListy.list.models import List, ListEntry
 
 
 @admin.register(List)
 class List(admin.ModelAdmin):
     list_display = ['title', 'user']
+
+
+@admin.register(ListEntry)
+class ListEntry(admin.ModelAdmin):
+    list_display = ['movie_id', 'movie_name', 'grade', 'would_recommend', 'user', 'list']
