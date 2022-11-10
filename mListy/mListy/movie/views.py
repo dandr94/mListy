00e055aa869c098a0eabaa62ef3cf700ebd3492a
dandr94 <d.andreev1994@gmail.com>
2022-11-10 19:32:37 +0200

    Added MovieDetailsView

--- mListy/mListy/movie/views.py
+++ mListy/mListy/movie/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
+from mListy.movie.models import MovieDB
 
-# Create your views here.
+
+class MovieDetailsView(LoginRequiredMixin, DetailView):
+    model = MovieDB
+    template_name = 'movie/details_movie.html'
+    context_object_name = 'movie'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
