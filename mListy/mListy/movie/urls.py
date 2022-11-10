2374463f01e052d6a892c068592e2b6131b83269
dandr94 <d.andreev1994@gmail.com>
2022-11-10 19:32:08 +0200

    Added movie details view in urls

--- mListy/mListy/movie/urls.py
+++ mListy/mListy/movie/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from mListy.movie.views import MovieDetailsView
+
 urlpatterns = [
+    path('movie/details/<str:slug>', MovieDetailsView.as_view(), name='details movie')
 
 ]
