dda27a4f1b844f4364f2c93a3bb36e29f77b7436
dandr94 <d.andreev1994@gmail.com>
2022-11-10 19:30:16 +0200

    Add details list view to urls

--- mListy/mListy/list/urls.py
+++ mListy/mListy/list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from mListy.list.views import CreateListView, EditListView, DeleteListView
+from mListy.list.views import CreateListView, EditListView, DeleteListView, DetailsListView
 
 urlpatterns = [
     path('create_list/', CreateListView.as_view(), name='create list'),
     path('list/edit/<str:slug>/', EditListView.as_view(), name='edit list'),
-    path('list/delete/<str:slug>/', DeleteListView.as_view(), name='delete list')
+    path('list/delete/<str:slug>/', DeleteListView.as_view(), name='delete list'),
+    path('list/details/<str:slug>/', DetailsListView.as_view(), name='details list')
 ]
