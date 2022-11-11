f010a49e35eb67c3fb8d8c884df2e8edb01fc65d
dandr94 <d.andreev1994@gmail.com>
2022-11-11 20:48:12 +0200

    Added two paths

--- mListy/mListy/movie/urls.py
+++ mListy/mListy/movie/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from mListy.movie.views import MovieDetailsView
+from mListy.movie.views import MovieDetailsView, AddMovieToListView
 
 urlpatterns = [
-    path('movie/details/<str:slug>', MovieDetailsView.as_view(), name='details movie')
+    path('movie/details/<str:slug>/', MovieDetailsView.as_view(), name='details movie'),
+    path('add-movie-to-list/<str:slug>/', AddMovieToListView.as_view(), name='add movie to list')
 
 ]
