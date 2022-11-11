3a49844683e21612f5094c8e71fc8c456f3612f7
dandr94 <d.andreev1994@gmail.com>
2022-11-11 20:47:05 +0200

    Change urls paths

--- mListy/mListy/list/urls.py
+++ mListy/mListy/list/urls.py
@@ -4,7 +4,7 @@ from mListy.list.views import CreateListView, EditListView, DeleteListView, Deta
 
 urlpatterns = [
     path('create_list/', CreateListView.as_view(), name='create list'),
-    path('list/edit/<str:slug>/', EditListView.as_view(), name='edit list'),
-    path('list/delete/<str:slug>/', DeleteListView.as_view(), name='delete list'),
-    path('list/details/<str:slug>/', DetailsListView.as_view(), name='details list')
+    path('<str:str>/list/edit/<str:slug>/', EditListView.as_view(), name='edit list'),
+    path('<str:str>/list/delete/<str:slug>/', DeleteListView.as_view(), name='delete list'),
+    path('<str:str>/list/details/<str:slug>/', DetailsListView.as_view(), name='details list')
 ]
