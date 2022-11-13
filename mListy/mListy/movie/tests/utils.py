870a168eb23fd1526f2c261f030168c74fe16db7
dandr94 <d.andreev1994@gmail.com>
2022-11-13 22:48:13 +0200

    Add list entry data

--- mListy/mListy/movie/tests/utils.py
+++ mListy/mListy/movie/tests/utils.py
@@ -15,3 +15,12 @@ VALID_MOVIEDB_DATA = {
     'release_date': '2022-12-08',
     'status': 'Released'
 }
+
+VALID_LIST_ENTRY_DATA = {
+    'movie_id': 54212,
+    'movie_name': 'FooBarBarz',
+    'grade': 5,
+    'would_recommend': 'Yes',
+
+}
+
