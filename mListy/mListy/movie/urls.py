507315af0725d1e3d7ee939475d14b78494301c1
dandr94 <d.andreev1994@gmail.com>
2022-11-13 17:17:52 +0200

    Added EditListEntryView and DeleteListEntryView urls

--- mListy/mListy/movie/urls.py
+++ mListy/mListy/movie/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from mListy.movie.views import MovieDetailsView, AddMovieToListView
+from mListy.movie.views import MovieDetailsView, AddListEntryView, EditListEntryView, DeleteListEntryView
 
 urlpatterns = [
     path('movie/details/<str:slug>/', MovieDetailsView.as_view(), name='details movie'),
-    path('add-movie-to-list/<str:slug>/', AddMovieToListView.as_view(), name='add movie to list')
-
+    path('add-movie-to-list/<str:slug>/', AddListEntryView.as_view(), name='add movie to list'),
+    path('edit/<int:pk>/<str:slug>/', EditListEntryView.as_view(), name='edit entry'),
+    path('delete/<int:pk>/<str:slug>/', DeleteListEntryView.as_view(), name='delete entry')
 ]
