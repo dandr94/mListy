5fa94c770ff1256a2fcffff044da8ca06dd84a76
dandr94 <d.andreev1994@gmail.com>
2022-11-14 20:51:09 +0200

    Added dispatch and SearchMovieView

--- mListy/mListy/movie/views.py
+++ mListy/mListy/movie/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.utils.text import slugify
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, TemplateView, ListView
 from mListy.list.models import List, ListEntry
 from mListy.movie.forms import AddListEntryForm, EditListEntryForm, DeleteListEntryForm
+from mListy.movie.helpers import check_if_in_db, add_movie_to_db
 from mListy.movie.models import MovieDB
+import tmdbsimple as tmdb
 
 
 class AddListEntryView(CreateView):
@@ -56,3 +60,48 @@ class MovieDetailsView(LoginRequiredMixin, DetailView):
     model = MovieDB
     template_name = 'movie/details_movie.html'
     context_object_name = 'movie'
+
+    def dispatch(self, request, *args, **kwargs):
+        movie_id = kwargs['slug'].split('-')[0]
+        exists = check_if_in_db(movie_id)
+        if not exists:
+            add_movie_to_db(movie_id)
+        kwargs['movie'] = exists
+        return super().dispatch(request, *args, **kwargs)
+
+
+class SearchMovieView(LoginRequiredMixin, ListView):
+    template_name = 'movie/search_movie.html'
+    IMG_PATH = 'https://image.tmdb.org/t/p/w500/'
+    IMG_NOT_FOUND = 'https://westsiderc.org/wp-content/uploads/2019/08/Image-Not-Available.png'
+
+    def get(self, request, *args, **kwargs):
+        keyword = request.GET.get('search', '')
+
+        if not keyword:
+            return render(request, self.template_name)
+
+        search_params = {
+            'query': keyword,
+            'page': 1,
+            'include_adult': False
+        }
+        search = tmdb.Search()
+        response = search.movie(**search_params)
+        movies = []
+
+        for item in response['results']:
+            item_result = {
+                'title': item['title'],
+                'vote_average': item['vote_average'],
+                'image': self.IMG_PATH + item['poster_path'] if item['poster_path'] else self.IMG_NOT_FOUND,
+                'slug': slugify(str(item['id']) + '-' + item['title'])
+            }
+
+            movies.append(item_result)
+
+        context = {
+            'movies': movies,
+        }
+
+        return render(request, self.template_name, context)
