e21daba7a6a4e92229cd9b1ffa0a358e06e8b25e
dandr94 <d.andreev1994@gmail.com>
2022-11-14 20:50:22 +0200

    Added SearchMovieView url

--- mListy/mListy/movie/urls.py
+++ mListy/mListy/movie/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from mListy.movie.views import MovieDetailsView, AddListEntryView, EditListEntryView, DeleteListEntryView
+from mListy.movie.views import MovieDetailsView, AddListEntryView, EditListEntryView, DeleteListEntryView, \
+    SearchMovieView
 
 urlpatterns = [
     path('movie/details/<str:slug>/', MovieDetailsView.as_view(), name='details movie'),
+    path('search/', SearchMovieView.as_view(), name='search movie'),
 
     path('add/<str:slug>/', AddListEntryView.as_view(), name='add entry'),
     path('edit/<int:pk>/<str:slug>/', EditListEntryView.as_view(), name='edit entry'),
