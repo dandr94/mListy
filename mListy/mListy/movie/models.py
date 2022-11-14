78ad78f778fc5dbf3547d6cf5e096ef764a09f43
dandr94 <d.andreev1994@gmail.com>
2022-11-14 20:50:01 +0200

    Rewrite model fields

--- mListy/mListy/movie/models.py
+++ mListy/mListy/movie/models.py
@@ -23,54 +23,73 @@ class MovieDB(models.Model):
     )
 
     poster = models.URLField(
-
+        blank=True,
+        null=True
     )
 
     description = models.TextField(
-
+        blank=True,
+        null=True
     )
 
     duration = models.IntegerField(
-
+        blank=True,
+        null=True
     )
 
     genres = models.CharField(
+        blank=True,
+        null=True,
         max_length=GENRES_MAX_CHAR
     )
 
     average_grade = models.FloatField(
-
+        blank=True,
+        null=True,
     )
 
     actors = models.CharField(
+        blank=True,
+        null=True,
         max_length=ACTORS_MAX_CHAR
     )
 
     roles = models.CharField(
+        blank=True,
+        null=True,
         max_length=ROLES_MAX_CHAR
     )
 
     production_companies = models.CharField(
+        blank=True,
+        null=True,
         max_length=PRODUCTION_COMPANIES_MAX_CHAR
     )
 
     language = models.CharField(
+        blank=True,
+        null=True,
         max_length=LANGUAGE_MAX_CHAR
     )
 
     imdb_link = models.URLField(
-
+        blank=True,
+        null=True,
     )
 
     budget = models.FloatField(
-
+        blank=True,
+        null=True,
     )
 
     release_date = models.DateField(
-
+        blank=True,
+        null=True,
     )
 
     status = models.CharField(
+        blank=True,
+        null=True,
         max_length=STATUS_MAX_CHAR
     )
 
