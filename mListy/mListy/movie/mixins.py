1f425512e1e9296f967d062ef8e454edf684b201
dandr94 <d.andreev1994@gmail.com>
2022-11-15 21:20:22 +0200

    Add mixin

---
+++ mListy/mListy/movie/mixins.py
@@ -0,0 +1,9 @@
+class CssStyleFormMixin:
+    fields = {}
+    def _init_css_style_form_controls(self):
+        for _, field in self.fields.items():
+            if not hasattr(field.widget, 'attrs'):
+                setattr(field.widget, 'attrs', {})
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = ''
+            field.widget.attrs['class'] += 'sign__input'
