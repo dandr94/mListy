0b93bd5101be1d5532f0dd4b2f54dc985b6f7eb1
dandr94 <d.andreev1994@gmail.com>
2022-11-17 22:30:14 +0200

    Added form for ChangePassword

--- mListy/mListy/account/forms.py
+++ mListy/mListy/account/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from mListy.account.models import Profile
 from mListy.movie.mixins import CssStyleFormMixin
 from django.forms import ModelForm
@@ -50,3 +50,17 @@ class EditListForm(CssStyleFormMixin, ModelForm):
         model = Profile
         fields = '__all__'
         exclude = ['slug', 'user']
+
+
+class ChangePasswordForm(CssStyleFormMixin, PasswordChangeForm):
+    FIELD_NAME_HELP_TEXT_TO_IGNORE = ['old_password', 'new_password1', 'new_password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_css_style_form_controls()
+        for field_name in self.FIELD_NAME_HELP_TEXT_TO_IGNORE:
+            self.fields[field_name].help_text = None
+
+    class Meta:
+        model = Profile
+        fields = ['old_password', 'new_password1', 'new_password2']
