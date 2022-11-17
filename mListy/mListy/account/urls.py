acb7864fd1146687142969fa99302fd616f2d9ff
dandr94 <d.andreev1994@gmail.com>
2022-11-17 22:30:31 +0200

    Added url for ChangePassword

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from mListy.account.views.auth import RegisterUserView, LoginUserView, LogoutUserView
 from mListy.account.views.generic import HomeViewNoProfile, Dashboard
-from mListy.account.views.profile import ProfileDetailsView, EditProfileView
+from mListy.account.views.profile import ProfileDetailsView, EditProfileView, ChangeUserPasswordView
 
 urlpatterns = [
     path('', HomeViewNoProfile.as_view(), name='index'),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('change_password/', ChangeUserPasswordView.as_view(), name='change password'),
 
     path('profile/<str:slug>/', ProfileDetailsView.as_view(), name='details profile'),
     path('profile/edit/<str:slug>/', EditProfileView.as_view(), name='edit profile')
