5d6040a5c7391eec7dec5d632d80efd3735ab5e6
dandr94 <d.andreev1994@gmail.com>
2022-11-19 22:50:13 +0200

    Update tests for HomeViewNoProfile

--- mListy/mListy/account/tests/views/test_HomeViewNoProfile.py
+++ mListy/mListy/account/tests/views/test_HomeViewNoProfile.py
@@ -1,42 +1,28 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS, VALID_LOGIN_CREDENTIALS
+from mListy.account.tests.BaseAccountTestClass import BaseAccountTestClass
 
-from mListy.account.models import Profile
-
-UserModel = get_user_model()
-
-
-class HomeViewNoProfileTests(TestCase):
-    PATH = '/'
-    INDEX_TEMPLATE = 'index.html'
+class HomeViewNoProfileTests(BaseAccountTestClass):
+    PATH = 'index'
+    TEMPLATE = 'index.html'
     DASHBOARD_TEMPLATE = 'dashboard.html'
 
+    def setUp(self) -> None:
+        self.user, self.profile = self.create_valid_user_and_profile()
+
     def test_user_is_not_authenticated__expect_correct_template(self):
-        response = self.client.get(self.PATH)
+        response = self.return_get_response()
 
-        self.assertTemplateUsed(response, self.INDEX_TEMPLATE)
+        self.assertTemplateUsed(response, self.TEMPLATE)
 
-    def test_user_is_authenticated__expect_correct_template(self):
-        user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        profile = Profile.objects.create(
-            user=user
-        )
-        profile.save()
-        self.client.login(**VALID_LOGIN_CREDENTIALS)
+    def test_user_is_authenticated__expect_correct_redirect_template(self):
+        self.login_user()
 
-        response = self.client.get(self.PATH, follow=True)
+        response = self.return_get_response(follow=True)
 
         self.assertTemplateUsed(response, self.DASHBOARD_TEMPLATE)
 
     def test_redirect_when_user_is_authenticated__expect_correct_status_code(self):
-        user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        profile = Profile.objects.create(
-            user=user
-        )
-        profile.save()
-        self.client.login(**VALID_LOGIN_CREDENTIALS)
+        self.login_user()
 
-        response = self.client.get(self.PATH)
+        response = self.return_get_response()
 
         self.assertEqual(response.status_code, 302)
