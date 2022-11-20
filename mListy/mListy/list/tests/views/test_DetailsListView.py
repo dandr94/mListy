f045dfa228c9cd059b0ca2c41047633d2475ce26
dandr94 <d.andreev1994@gmail.com>
2022-11-21 01:36:51 +0200

    Improve tests for DetailsListView

--- mListy/mListy/list/tests/views/test_DetailsListView.py
+++ mListy/mListy/list/tests/views/test_DetailsListView.py
@@ -1,68 +1,70 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls import reverse
-
-from mListy.account.models import Profile
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS, VALID_LOGIN_CREDENTIALS
-from mListy.list.models import List, ListEntry
+from mListy.list.models import ListEntry
+from mListy.list.tests.BaseListTestClass import BaseListTestClass
 from mListy.movie.models import MovieDB
-from mListy.movie.tests.utils import VALID_MOVIEDB_DATA
-
-UserModel = get_user_model()
+from mListy.movie.tests.utils import VALID_MOVIEDB_DATA, VALID_MOVIEDB_DATA_2
 
 
-class DetailsListViewTests(TestCase):
-    DETAILS_MOVIE_TEMPLATE = 'list/list_details.html'
-    VALID_LIST_TITLE_NAME = 'Drama'
+class DetailsListViewTests(BaseListTestClass):
+    TEMPLATE = 'list/details_list.html'
 
     VALID_ENTRY_DATA = {
-        'movie_id': 10,
-        'movie_name': 'Ice Age',
         'grade': 8,
-        'would_recommend': 'Yes'
+        'would_recommend': 'Yes',
+        'status': 'Completed'
     }
 
+    VALID_ENTRY_DATA_2 = {
+        'grade': 4,
+        'would_recommend': 'No',
+        'status': 'Dropped'
+    }
+
+    PATH = 'details list'
+
     def setUp(self):
-        self.user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        self.profile = Profile.objects.create(
-            user=self.user
-        )
-        self.profile.save()
-        self.client.login(**VALID_LOGIN_CREDENTIALS)
-        self.list = List.objects.create(title=self.VALID_LIST_TITLE_NAME, user=self.user)
-        self.list.save()
+        super().setUp()
+        self.user_list = self.create_list(self.user)
         self.movie = MovieDB.objects.create(**VALID_MOVIEDB_DATA)
-        self.movie.save()
-        self.entry = ListEntry.objects.create(**{
-            'movie_id': self.movie.id,
-            'movie_name': self.movie.name,
-            'grade': 5,
-            'would_recommend': 'No',
-            'list': self.list,
-            'user': self.user
-        })
-        self.entry2 = ListEntry.objects.create(**{
-            'movie_id': self.VALID_ENTRY_DATA['movie_id'],
-            'movie_name': self.VALID_ENTRY_DATA['movie_name'],
-            'grade': self.VALID_ENTRY_DATA['grade'],
-            'would_recommend': self.VALID_ENTRY_DATA['would_recommend'],
-            'list': self.list,
-            'user': self.user
-        })
-        self.path = reverse('details list', kwargs={'str': self.user.username, 'slug': self.list.slug})
+        self.movie_2 = MovieDB.objects.create(**VALID_MOVIEDB_DATA_2)
+        self.entry = ListEntry.objects.create(**self.VALID_ENTRY_DATA, movie=self.movie, list=self.user_list)
+        self.entry2 = ListEntry.objects.create(**self.VALID_ENTRY_DATA_2, movie=self.movie_2, list=self.user_list)
 
     def test_correct_template_is_used(self):
-        response = self.client.get(self.path)
+        response = self.get_response_for_list(self.user_list, self.profile)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, self.DETAILS_MOVIE_TEMPLATE)
+        self.assertTemplateUsed(response, self.TEMPLATE)
 
     def test_correct_data_is_listed(self):
-        response = self.client.get(self.path)
+        response = self.get_response_for_list(self.user_list, self.profile)
 
         entries = [self.entry, self.entry2]
-        for i in range(len(entries)):
-            self.assertEqual(response.context['movie_list'][i].movie_id, entries[i].movie_id)
-            self.assertEqual(response.context['movie_list'][i].movie_name, entries[i].movie_name)
-            self.assertEqual(response.context['movie_list'][i].grade, entries[i].grade)
-            self.assertEqual(response.context['movie_list'][i].would_recommend, entries[i].would_recommend)
+        for i, v in enumerate(response.context['movie_list'].listentry_set.all()):
+            self.assertEqual(v.grade, entries[i].grade)
+            self.assertEqual(v.would_recommend, entries[i].would_recommend)
+            self.assertEqual(v.status, entries[i].status)
+            self.assertEqual(v.movie, entries[i].movie)
+            self.assertEqual(v.list, entries[i].list)
 
+    def test_total_movies_stats(self):
+        response = self.get_response_for_list(self.user_list, self.profile)
+
+        expected_total_movie_value = 2
+
+        self.assertEqual(len(response.context['movie_list'].listentry_set.all()), expected_total_movie_value)
+
+    def test_time_spend_watching_stats(self):
+        response = self.get_response_for_list(self.user_list, self.profile)
+
+        minutes = self.movie.duration + self.movie_2.duration
+        expected_hours = minutes // 60
+        expected_days = expected_hours // 24
+
+        self.assertEqual(response.context['total_time_hours'], expected_hours)
+        self.assertEqual(response.context['total_time_days'], expected_days)
+
+    def test_average_grade_stats(self):
+        response = self.get_response_for_list(self.user_list, self.profile)
+
+        expected_average_grade = (self.entry.grade + self.entry2.grade) // 2
+
+        self.assertEqual(response.context['average_grade'], expected_average_grade)
