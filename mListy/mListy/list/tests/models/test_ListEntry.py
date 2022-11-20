13b998b9f36622a4c7c2b3793bd2c1a01c9a226c
dandr94 <d.andreev1994@gmail.com>
2022-11-21 01:35:51 +0200

    Improve tests for ListEntry

--- mListy/mListy/list/tests/models/test_ListEntry.py
+++ mListy/mListy/list/tests/models/test_ListEntry.py
@@ -1,53 +1,38 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-
-from mListy.account.models import Profile
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS
-from mListy.list.models import List, ListEntry
+from mListy.list.models import ListEntry
+from mListy.list.tests.BaseListTestClass import BaseListTestClass
 from mListy.movie.models import MovieDB
 from mListy.movie.tests.utils import VALID_MOVIEDB_DATA
 
-UserModel = get_user_model()
 
-
-class ListEntryTests(TestCase):
+class ListEntryTests(BaseListTestClass):
     VALID_LIST_TITLE_NAME = 'Drama'
 
     def setUp(self):
-        self.user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        self.profile = Profile.objects.create(
-            user=self.user
-        )
-        self.profile.save()
-        self.list = List.objects.create(title=self.VALID_LIST_TITLE_NAME, user=self.user)
-        self.list.save()
+        super().setUp()
+        self.user_list = self.create_list(self.user)
         self.movie = MovieDB.objects.create(**VALID_MOVIEDB_DATA)
-        self.movie.save()
-        self.valid_entry_data = {'movie_id': 5,
-                                 'movie_name': 'Hello',
-                                 'grade': 5,
-                                 'list': self.list,
-                                 'user': self.user}
+        self.valid_entry_data = {
+            'grade': 5,
+            'list': self.user_list,
+            'movie': self.movie}
 
     def test_create_list_with_valid_data__expect_correct_values(self):
-        entry = ListEntry(**self.valid_entry_data)
+        ListEntry.objects.create(**self.valid_entry_data)
 
-        entry.save()
+        entry = ListEntry.objects.get(list_id=self.user_list.id)
 
-        self.assertIsNotNone(entry.pk)
+        self.assertIsNotNone(entry)
 
     def test_create_list_with_valid_data__expect_slug_to_be_correct_value(self):
-        entry = ListEntry(**self.valid_entry_data)
+        entry = ListEntry.objects.create(**self.valid_entry_data)
 
-        entry.save()
-        expected_slug_value = '5-hello'
+        expected_slug_value = '54212-foobarbarz'
 
         self.assertEqual(expected_slug_value, entry.slug)
 
     def test_return_title(self):
-        entry = ListEntry(**self.valid_entry_data)
+        entry = ListEntry.objects.create(**self.valid_entry_data)
 
-        entry.save()
-        expected_title_value = 'Hello'
+        expected_title_value = 'FooBarBarz'
 
         self.assertEqual(expected_title_value, str(entry))
