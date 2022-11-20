f6ae6832f9d79867e01599ca1cf1ab0062ac9f71
dandr94 <d.andreev1994@gmail.com>
2022-11-21 01:36:30 +0200

    Improve tests for DeleteListView

--- mListy/mListy/list/tests/views/test_DeleteListView.py
+++ mListy/mListy/list/tests/views/test_DeleteListView.py
@@ -1,57 +1,44 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
 from django.urls import reverse
-
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS, VALID_LOGIN_CREDENTIALS
-
-from mListy.account.models import Profile
 from mListy.list.models import List
+from mListy.list.tests.BaseListTestClass import BaseListTestClass
 
-UserModel = get_user_model()
 
-
-class CreateListFormTests(TestCase):
+class DeleteListViewTests(BaseListTestClass):
     VALID_LIST_DATA = {
         'title': 'Drama'
     }
-    DELETE_LIST_TEMPLATE = 'list/delete_list.html'
+    TEMPLATE = 'list/delete_list.html'
 
-    VALID_TITLE_NAME = 'Drama'
-
-    FORM = 'form'
+    PATH = 'delete list'
 
     def setUp(self):
-        self.user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        self.profile = Profile.objects.create(
-            user=self.user
-        )
-        self.profile.save()
-        self.client.login(**VALID_LOGIN_CREDENTIALS)
-        self.list = List(title=self.VALID_TITLE_NAME, user=self.user)
-        self.list.save()
-        self.path = reverse('delete list', kwargs={'str': self.user.username, 'slug': self.list.slug})
-
-    def __get_post_response(self, credentials):
-        return self.client.post(self.path, args=credentials)
-
-    def __get_get_response(self):
-        return self.client.get(self.path)
+        super().setUp()
+        self.user_list = self.create_list(self.user)
 
     def test_correct_template_is_used(self):
-        response = self.__get_get_response()
+        response = self.client.get(reverse(self.PATH, kwargs={'str': self.profile.slug, 'slug': self.user_list.slug}))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, self.DELETE_LIST_TEMPLATE)
+        self.assertTemplateUsed(response, self.TEMPLATE)
 
     def test_delete_list(self):
-        response = self.__get_post_response(self.list.id)
+        user_list = List.objects.first()
 
-        user_list = List.objects.exists()
+        self.assertTrue(user_list)
+
+        self.post_response_for_list(self.user_list, self.profile, {})
+
+        user_list = List.objects.first()
 
         self.assertFalse(user_list)
 
-    def test_redirect_after_deletion(self):
-        response = self.__get_post_response(self.list.id)
+    def test_status_code_after_deletion__expect_302(self):
+        response = self.post_response_for_list(self.user_list, self.profile, {})
 
-        expected_redirect_url = '/dashboard/'
+        self.assertEqual(response.status_code, 302)
+
+    def test_redirect_after_deletion(self):
+        response = self.post_response_for_list(self.user_list, self.profile, {})
+
+        expected_redirect_url = reverse('dashboard')
 
         self.assertRedirects(response, expected_redirect_url)
