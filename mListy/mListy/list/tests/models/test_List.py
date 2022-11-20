2f3516cd013286257c4db1f42ced5b4a74c5cc13
dandr94 <d.andreev1994@gmail.com>
2022-11-21 01:35:36 +0200

    Improve tests for List

--- mListy/mListy/list/tests/models/test_List.py
+++ mListy/mListy/list/tests/models/test_List.py
@@ -1,42 +1,30 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-
-from mListy.account.models import Profile
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS
 from mListy.list.models import List
+from mListy.list.tests.BaseListTestClass import BaseListTestClass
 
-UserModel = get_user_model()
 
-
-class ListTests(TestCase):
+class ListTests(BaseListTestClass):
     VALID_TITLE_NAME = 'Drama'
 
     def setUp(self):
-        self.user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        self.profile = Profile.objects.create(
-            user=self.user
-        )
-        self.profile.save()
+        super().setUp()
 
     def test_create_list_with_valid_data__expect_correct_values(self):
-        movie_list = List(title=self.VALID_TITLE_NAME, user=self.user)
+        List.objects.create(title=self.VALID_TITLE_NAME, user=self.user)
 
-        movie_list.save()
+        user_list = List.objects.get(title=self.VALID_TITLE_NAME, user=self.user)
 
-        self.assertIsNotNone(movie_list.pk)
+        self.assertIsNotNone(user_list)
 
     def test_create_list_with_valid_data__expect_slug_to_be_correct_value(self):
-        movie_list = List(title=self.VALID_TITLE_NAME, user=self.user)
+        user_list = List.objects.create(title=self.VALID_TITLE_NAME, user=self.user)
 
-        movie_list.save()
         expected_slug_value = 'drama'
 
-        self.assertEqual(expected_slug_value, movie_list.slug)
+        self.assertEqual(expected_slug_value, user_list.slug)
 
     def test_return_title(self):
-        movie_list = List(title=self.VALID_TITLE_NAME, user=self.user)
+        user_list = List.objects.create(title=self.VALID_TITLE_NAME, user=self.user)
 
-        movie_list.save()
         expected_title_value = 'Drama'
 
-        self.assertEqual(expected_title_value, str(movie_list))
+        self.assertEqual(expected_title_value, str(user_list))
