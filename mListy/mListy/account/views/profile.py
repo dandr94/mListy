83d2ebde0934f25bdaf8f10ad942ffdaf3f26b99
dandr94 <d.andreev1994@gmail.com>
2022-11-20 22:44:37 +0200

    Profile details now require less queries and shows last added entries

--- mListy/mListy/account/views/profile.py
+++ mListy/mListy/account/views/profile.py
@@ -3,11 +3,10 @@ from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, UpdateView
 
-from mListy.account.forms import EditListForm, ChangePasswordForm
+from mListy.account.forms import EditProfileForm, ChangePasswordForm
 from mListy.account.mixins import PermissionHandlerMixin
 from mListy.account.models import Profile
-from mListy.list.models import List, ListEntry
-from mListy.movie.helpers import return_list_with_additional_stats
+from mListy.list.models import List
 
 
 class ProfileDetailsView(DetailView):
@@ -18,18 +17,20 @@ class ProfileDetailsView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_owner'] = self.object.user_id == self.request.user.id
-        context['user_lists'] = List.objects.filter(user_id=self.object.user_id)
-        context['entries'] = ListEntry.objects.filter(list__user_id=self.object.user.id)
-        context['total_average_grade'] = sum([e.grade for e in context['entries']]) // len(context['entries'])
-        context['user_lists_dict'] = return_list_with_additional_stats(context['user_lists'], context['entries'])
-
+        context['user_lists'] = List.objects.prefetch_related('listentry_set__movie').filter(
+            user_id=self.object.user_id)
+        entries_dict = {x: x.grade for i in context['user_lists'] for x in i.listentry_set.all()}
+        context['total_movies'] = len(entries_dict)
+        context['total_average_grade'] = sum(entries_dict.values()) // context['total_movies'] \
+            if context['total_movies'] else 0
+        context['last_added'] = sorted(entries_dict.items(), key=lambda x: x[0].date_created, reverse=True)[0:5]
         return context
 
 
 class EditProfileView(LoginRequiredMixin, PermissionHandlerMixin, UpdateView):
     model = Profile
     template_name = 'account/edit_profile.html'
-    form_class = EditListForm
+    form_class = EditProfileForm
 
     def get_success_url(self):
         return reverse_lazy('details profile', kwargs={'slug': self.object.slug})
