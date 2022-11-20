d9bc66c013a132c57085d4a3f68a2e9f26919cd8
dandr94 <d.andreev1994@gmail.com>
2022-11-21 01:36:13 +0200

    Improve tests for CreateListView

--- mListy/mListy/list/tests/views/test_CreateListView.py
+++ mListy/mListy/list/tests/views/test_CreateListView.py
@@ -1,44 +1,40 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-
-from mListy.account.models import Profile
-from mListy.account.tests.utils import VALID_USER_CREDENTIALS, VALID_LOGIN_CREDENTIALS
+from django.urls import reverse
+from mListy.account.tests.utils import VALID_LOGIN_CREDENTIALS
 from mListy.list.models import List
+from mListy.list.tests.BaseListTestClass import BaseListTestClass
 
-UserModel = get_user_model()
 
+class CreateListViewTests(BaseListTestClass):
+    PATH = 'create list'
 
-class CreateListViewTests(TestCase):
-    PATH = '/create_list/'
-    CREATE_LIST_TEMPLATE = 'list/create_list.html'
+    TEMPLATE = 'list/create_list.html'
 
     VALID_LIST_DATA = {
         'title': 'Drama'
     }
 
     def setUp(self):
-        self.user = UserModel.objects.create_user(**VALID_USER_CREDENTIALS)
-        self.profile = Profile.objects.create(
-            user=self.user
-        )
-        self.profile.save()
+        super().setUp()
         self.client.login(**VALID_LOGIN_CREDENTIALS)
 
     def test_correct_template_is_used(self):
-        response = self.client.get(self.PATH)
+        response = self.client.get(reverse(self.PATH))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, self.CREATE_LIST_TEMPLATE)
+        self.assertTemplateUsed(response, self.TEMPLATE)
 
     def test_create_list_with_valid_credentials(self):
-        self.client.post(self.PATH, data=self.VALID_LIST_DATA)
+        self.client.post(reverse(self.PATH), data=self.VALID_LIST_DATA)
 
         user_list = List.objects.get(title=self.VALID_LIST_DATA['title'], user=self.user)
 
         self.assertIsNotNone(user_list)
         self.assertEqual(self.VALID_LIST_DATA['title'], user_list.title)
 
-    def test_redirect_after_valid_list_creation(self):
-        response = self.client.post(self.PATH, self.VALID_LIST_DATA)
-        expected_url = '/dashboard/'
+    def test_status_code_after_valid_list_creation__expect_302(self):
+        response = self.client.post(reverse(self.PATH), self.VALID_LIST_DATA)
         self.assertEqual(response.status_code, 302)
+
+    def test_redirect_after_valid_list_creation(self):
+        response = self.client.post(reverse(self.PATH), self.VALID_LIST_DATA)
+        expected_url = '/dashboard/'
         self.assertRedirects(response, expected_url)
