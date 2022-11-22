34cfbf04c1454de5a07965e1d3309b3570f87dfe
dandr94 <d.andreev1994@gmail.com>
2022-11-22 07:41:33 +0200

    Removed ListEntry urls

--- mListy/mListy/movie/urls.py
+++ mListy/mListy/movie/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 
-from mListy.movie.views import MovieDetailsView, AddListEntryView, EditListEntryView, DeleteListEntryView, \
-    SearchMovieView
+from mListy.movie.views import MovieDetailsView, SearchMovieView
 
 urlpatterns = [
     path('movie/details/<str:slug>/', MovieDetailsView.as_view(), name='details movie'),
     path('search/', SearchMovieView.as_view(), name='search movie'),
-
-    path('add/<str:slug>/', AddListEntryView.as_view(), name='add entry'),
-    path('edit/<int:pk>/<str:slug>/', EditListEntryView.as_view(), name='edit entry'),
-    path('delete/<int:pk>/<str:slug>/', DeleteListEntryView.as_view(), name='delete entry')
 ]
