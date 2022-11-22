3fa7cd3a4a5514fca8ee140935991e2b162fb144
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:50:14 +0200

    Improve tests

--- mListy/mListy/movie/tests/models/test_MovieDB.py
+++ mListy/mListy/movie/tests/models/test_MovieDB.py
@@ -1,19 +1,28 @@
-from django.test import TestCase
+from mListy.movie.models import MovieDB
+from mListy.movie.tests.BaseMovieTestClass import BaseMovieTestClass
 from mListy.movie.tests.utils import VALID_MOVIEDB_DATA
 
-from mListy.movie.models import MovieDB
 
-
-class MovieDBTests(TestCase):
-
-    def setUp(self):
-        self.movie = MovieDB(**VALID_MOVIEDB_DATA)
-        self.movie.save()
+class MovieDBTests(BaseMovieTestClass):
 
     def test_movie_with_valid_credentials__expect_valid_save(self):
-        self.assertIsNotNone(self.movie.pk)
+        movie = MovieDB.objects.first()
+
+        self.assertIsNone(movie)
+
+        movie = self.create_movie()
+
+        self.assertIsNotNone(movie.pk)
+
+    def test_movie_values(self):
+        movie = self.create_movie()
+
+        for k in VALID_MOVIEDB_DATA.keys():
+            self.assertEqual(getattr(movie, k), VALID_MOVIEDB_DATA[k])
 
     def test_return_title(self):
-        expected_title_value = 'FooBarBarz'
-        self.assertEqual(expected_title_value, str(self.movie))
+        movie = self.create_movie()
 
+        expected_title_value = 'FooBarBarz'
+
+        self.assertEqual(expected_title_value, str(movie))
