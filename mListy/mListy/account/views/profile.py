f475eae2bb53be390f7d18156a3517fc91b98a71
dandr94 <d.andreev1994@gmail.com>
2022-11-22 07:48:14 +0200

    Improve ProfileDetailsView

--- mListy/mListy/account/views/profile.py
+++ mListy/mListy/account/views/profile.py
@@ -7,6 +7,7 @@ from mListy.account.forms import EditProfileForm, ChangePasswordForm
 from mListy.account.mixins import PermissionHandlerMixin
 from mListy.account.models import Profile
 from mListy.list.models import List
+from mListy.movie.helpers import return_last_added_entries, return_total_average_grade
 
 
 class ProfileDetailsView(DetailView):
@@ -17,13 +18,13 @@ class ProfileDetailsView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_owner'] = self.object.user_id == self.request.user.id
-        context['user_lists'] = List.objects.prefetch_related('listentry_set__movie').filter(
-            user_id=self.object.user_id)
+        context['user_lists'] = List.objects \
+            .prefetch_related('listentry_set__movie') \
+            .filter(user_id=self.object.user_id)
         entries_dict = {x: x.grade for i in context['user_lists'] for x in i.listentry_set.all()}
         context['total_movies'] = len(entries_dict)
-        context['total_average_grade'] = sum(entries_dict.values()) // context['total_movies'] \
-            if context['total_movies'] else 0
-        context['last_added'] = sorted(entries_dict.items(), key=lambda x: x[0].date_created, reverse=True)[0:5]
+        context['total_average_grade'] = return_total_average_grade(entries_dict)
+        context['last_added'] = return_last_added_entries(entries_dict)
         return context
 
 
