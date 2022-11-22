b53d0e777f2baceb2e5e39e67b2d86fffd41528f
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:27:55 +0200

    Moved AddListEntryView tests

--- mListy/mListy/list_entry/tests/views/test_AddListEntryView.py
+++ mListy/mListy/list_entry/tests/views/test_AddListEntryView.py
@@ -1,40 +1,46 @@
 from django.urls import reverse
 from mListy.list_entry.models import ListEntry
-from mListy.movie.tests.BaseMovieTest import BaseMovieTest
+from mListy.list_entry.tests.BaseListEntryTestClass import BaseListEntryTestClass
 
 
-class AddListEntryViewTests(BaseMovieTest):
+class AddListEntryViewTests(BaseListEntryTestClass):
     TEMPLATE = 'movie/add_entry.html'
 
     PATH = 'add entry'
 
+    VALID_LIST_ENTRY_DATA_2 = {
+        'grade': 8,
+        'would_recommend': 'No',
+        'status': 'Dropped'
+    }
+
     def setUp(self):
         super().setUp()
-        self.path = self.get_path(self.PATH, {'slug': self.movie.slug})
-        self.valid_data = {
-            'grade': 8,
-            'list': self.list.id,
-            'would_recommend': 'Yes',
-            'status': 'Completed',
-            'movie': self.movie
-        }
-        ListEntry.objects.all().delete()
+        self.VALID_LIST_ENTRY_DATA_2['list'] = self.user_list.id
+        self.VALID_LIST_ENTRY_DATA_2['movie'] = self.movie
 
     def test_correct_template_is_used(self):
-        response = self.return_get_response()
+        response = self.client.get(reverse(self.PATH, kwargs={'slug': self.movie.slug}))
+
         self.assertEqual(response.status_code, 200)
+
         self.assertTemplateUsed(response, self.TEMPLATE)
 
     def test_add_entry_to_list_with_valid_credentials(self):
-        self.return_post_response(self.valid_data)
-        entry = ListEntry.objects.get(slug=self.movie.slug, list__id=self.list.id)
+        self.post_response_for_create_list_entry(self.movie, self.VALID_LIST_ENTRY_DATA_2)
+
+        entry = ListEntry.objects.get(slug=self.movie.slug, list__id=self.user_list.id)
+
         self.assertIsNotNone(entry)
 
     def test_status_code_after_valid_add_entry_to_list_operation__expect_302(self):
-        response = self.return_post_response(self.valid_data)
+        response = self.post_response_for_create_list_entry(self.movie, self.VALID_LIST_ENTRY_DATA_2)
+
         self.assertEqual(response.status_code, 302)
 
     def test_redirect_after_valid_add_entry_to_list_operation(self):
-        response = self.return_post_response(self.valid_data)
+        response = self.post_response_for_create_list_entry(self.movie, self.VALID_LIST_ENTRY_DATA_2)
+
         expected_url = reverse('details movie', kwargs={'slug': self.movie.slug})
+
         self.assertRedirects(response, expected_url)
