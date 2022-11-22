e29ff16fd7797499efb2820afe96134ed3fbea52
dandr94 <d.andreev1994@gmail.com>
2022-11-22 07:43:15 +0200

    Removed ListEntry

--- mListy/mListy/movie/views.py
+++ mListy/mListy/movie/views.py
@@ -1,59 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.utils.text import slugify
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
-from mListy.list.models import List, ListEntry
-from mListy.movie.forms import AddListEntryForm, EditListEntryForm, DeleteListEntryForm
+from django.views.generic import DetailView, ListView
 from mListy.movie.helpers import check_if_in_db, add_movie_to_db, return_youtube_trailer
 from mListy.movie.models import MovieDB
 import tmdbsimple as tmdb
-
-
-class AddListEntryView(CreateView):
-    template_name = 'movie/add_entry.html'
-    form_class = AddListEntryForm
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
-        kwargs['user_lists'] = List.objects.filter(user_id=self.request.user.id)
-        kwargs['movie'] = MovieDB.objects.get(slug=self.kwargs['slug'])
-
-        return kwargs
-
-    def get_success_url(self):
-        return reverse_lazy('details movie', kwargs={'slug': self.kwargs['slug']})
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['movie_slug'] = self.kwargs['slug']
-        return context
-
-
-class EditListEntryView(LoginRequiredMixin, UpdateView):
-    model = ListEntry
-    template_name = 'movie/edit_entry.html'
-    form_class = EditListEntryForm
-    context_object_name = 'entry'
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user_lists'] = List.objects.filter(user_id=self.request.user.id)
-        return kwargs
-
-    def get_success_url(self):
-        return reverse_lazy('details list', kwargs={'str': self.request.user.username, 'slug': self.object.list.slug})
-
-
-class DeleteListEntryView(DeleteView):
-    model = ListEntry
-    template_name = 'movie/delete_entry.html'
-    context_object_name = 'entry'
-    form_class = DeleteListEntryForm
-
-    def get_success_url(self):
-        return reverse_lazy('details list', kwargs={'str': self.request.user.username, 'slug': self.object.list.slug})
 
 
 class MovieDetailsView(LoginRequiredMixin, DetailView):
@@ -71,7 +22,8 @@ class MovieDetailsView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['youtube_trailer'] = return_youtube_trailer(context['movie'].name, str(context['movie'].release_date.year))
+        context['youtube_trailer'] = return_youtube_trailer(context['movie'].name,
+                                                            str(context['movie'].release_date.year))
         return context
 
 
