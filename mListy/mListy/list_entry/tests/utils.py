ba282b15b7ce9eb3da6d3bc902f48e9188482bd5
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:25:23 +0200

    Moved ListEntry utils

---
+++ mListy/mListy/list_entry/tests/utils.py
@@ -0,0 +1,11 @@
+VALID_LIST_ENTRY_DATA = {
+    'grade': 4,
+    'would_recommend': 'Yes',
+    'status': 'Completed'
+
+}
+VALID_LIST_ENTRY_DATA_2 = {
+    'grade': 8,
+    'would_recommend': 'No',
+    'status': 'Dropped'
+}
