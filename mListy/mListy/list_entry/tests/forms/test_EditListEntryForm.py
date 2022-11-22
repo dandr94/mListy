9cfa10f4f9bf2b3c271138af8166cc2cee1618cc
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:26:35 +0200

    Moved EditListEntryForm tests

--- mListy/mListy/list_entry/tests/forms/test_EditListEntryForm.py
+++ mListy/mListy/list_entry/tests/forms/test_EditListEntryForm.py
@@ -1,34 +1,39 @@
-from mListy.movie.tests.BaseMovieTest import BaseMovieTest
+from mListy.list_entry.tests.BaseListEntryTestClass import BaseListEntryTestClass
 
 
-class EditListEntryFormTests(BaseMovieTest):
+class EditListEntryFormTests(BaseListEntryTestClass):
     PATH = 'edit entry'
     EMPTY_FIELD_ERROR_MSG = 'This field is required.'
 
-    INVALID_GRADE_FIELD_DATA = {
-        'grade': '',
-        'list': 'Drama',
-        'would_recommend': 'Yes'
-    }
-
-    INVALID_LIST_FIELD_DATA = {
-        'grade': 5,
-        'list': '',
-        'would_recommend': 'Yes'
-    }
-
     def setUp(self):
         super().setUp()
-        self.path = self.get_path(self.PATH, {'pk': self.entry.pk, 'slug': self.entry.slug})
+        self.entry = self.create_entry()
 
     def test_empty_grade_field__should_return_correct_error_msg(self):
         field_name_key = 'grade'
 
-        response = self.return_post_response(self.INVALID_GRADE_FIELD_DATA)
+        self.FIELD_DATA['grade'] = ''
+
+        response = self.post_response_for_list_entry(self.entry, self.FIELD_DATA)
+
         self.assertFormError(response, self.FORM, field_name_key, self.EMPTY_FIELD_ERROR_MSG)
 
     def test_empty_list_field__should_return_correct_error_msg(self):
         field_name_key = 'list'
 
-        response = self.return_post_response(self.INVALID_LIST_FIELD_DATA)
+        self.FIELD_DATA['list'] = ''
+
+        response = self.post_response_for_list_entry(self.entry, self.FIELD_DATA)
+
         self.assertFormError(response, self.FORM, field_name_key, self.EMPTY_FIELD_ERROR_MSG)
+
+    def test_empty_status_field__should_return_correct_error_msg(self):
+        field_name_key = 'status'
+
+        self.FIELD_DATA['status'] = ''
+
+        response = self.post_response_for_list_entry(self.entry, self.FIELD_DATA)
+
+        self.assertFormError(response, self.FORM, field_name_key, self.EMPTY_FIELD_ERROR_MSG)
+
+
