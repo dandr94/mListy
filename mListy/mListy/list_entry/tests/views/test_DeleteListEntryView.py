ac9912d8a8eded812d1235e16ca2e958854d9c46
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:28:35 +0200

    Moved DeleteListEntryView tests

--- mListy/mListy/list_entry/tests/views/test_DeleteListEntryView.py
+++ mListy/mListy/list_entry/tests/views/test_DeleteListEntryView.py
@@ -1,20 +1,20 @@
 from django.urls import reverse
 
 from mListy.list_entry.models import ListEntry
-from mListy.movie.tests.BaseMovieTest import BaseMovieTest
+from mListy.list_entry.tests.BaseListEntryTestClass import BaseListEntryTestClass
 
 
-class DeleteListEntryViewTests(BaseMovieTest):
+class DeleteListEntryViewTests(BaseListEntryTestClass):
     TEMPLATE = 'movie/delete_entry.html'
 
     PATH = 'delete entry'
 
     def setUp(self):
         super().setUp()
-        self.path = self.get_path(self.PATH, {'pk': self.entry.pk, 'slug': self.entry.slug})
+        self.entry = self.create_entry()
 
     def test_correct_template_is_used(self):
-        response = self.return_get_response()
+        response = self.get_response_for_list_entry(self.entry)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, self.TEMPLATE)
 
@@ -23,15 +23,15 @@ class DeleteListEntryViewTests(BaseMovieTest):
 
         self.assertTrue(entry)
 
-        self.return_post_response({})
+        self.post_response_for_list_entry(self.entry, {})
 
         entry = ListEntry.objects.exists()
 
         self.assertFalse(entry)
 
     def test_redirect_after_deletion(self):
-        response = self.return_post_response({})
+        response = self.post_response_for_list_entry(self.entry, {})
 
-        expected_redirect_url = reverse('details list', kwargs={'str': self.user.username, 'slug': self.list.slug})
+        expected_redirect_url = reverse('details list', kwargs={'str': self.user.username, 'slug': self.user_list.slug})
 
         self.assertRedirects(response, expected_redirect_url)
