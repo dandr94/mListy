b87a08d9e4ab3453d75ebde9f573af20ae4eaaae
dandr94 <d.andreev1994@gmail.com>
2022-11-22 09:33:18 +0200

    Add more moviedb data and rremove entry_data

--- mListy/mListy/movie/tests/utils.py
+++ mListy/mListy/movie/tests/utils.py
@@ -16,11 +16,20 @@ VALID_MOVIEDB_DATA = {
     'status': 'Released'
 }
 
-VALID_LIST_ENTRY_DATA = {
-    'movie_id': 54212,
-    'movie_name': 'FooBarBarz',
-    'grade': 5,
-    'would_recommend': 'Yes',
-
+VALID_MOVIEDB_DATA_2 = {
+    'movie_id': 1337,
+    'name': 'leetlizard',
+    'poster': 'https://foo.bar',
+    'description': 'Lorem ipsum un mass foo bar.',
+    'duration': 155,
+    'genres': 'Action, Drama',
+    'average_grade': 9.5,
+    'actors': 'Foo, Bar, Barz',
+    'roles': 'Barz, Bar, Foo',
+    'production_companies': 'FromBarzSoftware',
+    'language': 'En',
+    'imdb_link': 'https://foo.bar',
+    'budget': 1500000.0,
+    'release_date': '2022-12-08',
+    'status': 'Released'
 }
-
