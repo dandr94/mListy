be86ec0b734cc682bd918dad2c8d569972827faf
dandr94 <d.andreev1994@gmail.com>
2022-11-27 18:49:08 +0200

    Fix urls

--- mListy/mListy/urls.py
+++ mListy/mListy/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mListy.account.urls')),
-    path('', include('mListy.list.urls')),
-    path('', include('mListy.movie.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('mListy.account.urls')),
+                  path('', include('mListy.list.urls')),
+                  path('', include('mListy.list_entry.urls')),
+                  path('', include('mListy.movie.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
