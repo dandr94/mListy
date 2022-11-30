2ff90c6ef8c2d45bb49da1195494ab8c7f949fdb
dandr94 <d.andreev1994@gmail.com>
2022-11-30 16:38:52 +0200

    Add EntryPermissionHandlerMixin

--- mListy/mListy/account/mixins.py
+++ mListy/mListy/account/mixins.py
@@ -8,3 +8,10 @@ class PermissionHandlerMixin:
         if not owner and not request.user.is_superuser:
             raise PermissionDenied
         return response
+
+class EntryPermissionHandlerMixin:
+    def dispatch(self, request, *args, **kwargs):
+        owner = self.request.user.id == self.get_object().list.user_id
+        if not owner and not request.user.is_superuser:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
