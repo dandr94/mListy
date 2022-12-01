051de4ff89f1350b0c1085756bcf19010fa2b1f7
dandr94 <d.andreev1994@gmail.com>
2022-12-01 23:44:36 +0200

    Add test

--- mListy/mListy/list_entry/tests/forms/test_EditListEntryForm.py
+++ mListy/mListy/list_entry/tests/forms/test_EditListEntryForm.py
@@ -1,4 +1,7 @@
+from mListy.list.models import List
+from mListy.list_entry.models import ListEntry
 from mListy.list_entry.tests.BaseListEntryTestClass import BaseListEntryTestClass
+from mListy.list_entry.tests.utils import VALID_LIST_ENTRY_DATA
 
 
 class EditListEntryFormTests(BaseListEntryTestClass):
@@ -36,4 +39,16 @@ class EditListEntryFormTests(BaseListEntryTestClass):
 
         self.assertFormError(response, self.FORM, field_name_key, self.EMPTY_FIELD_ERROR_MSG)
 
+    def test_adding_the_entry_to_another_list_with_existing_identical_entry__expect_correct_error_msg(self):
+        mock_list = List.objects.create(title='Mock', user=self.user)
+        mock_entry = ListEntry.objects.create(**VALID_LIST_ENTRY_DATA, movie=self.movie, list=mock_list)
 
+        self.assertTrue(mock_entry.pk)
+
+        self.FIELD_DATA['list'] = self.user_list.id
+
+        response = self.post_response_for_list_entry(mock_entry, self.FIELD_DATA)
+
+        expected_error_msg = f'{self.movie.name} is already in {self.user_list}.'
+
+        self.assertFormError(response, self.FORM, None, expected_error_msg)
