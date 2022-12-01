3d39c74d405f78028d430296f645657958078224
dandr94 <d.andreev1994@gmail.com>
2022-12-01 23:42:51 +0200

    add 403 test

--- mListy/mListy/list/tests/views/test_EditListView.py
+++ mListy/mListy/list/tests/views/test_EditListView.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+
 from mListy.list.tests.BaseListTestClass import BaseListTestClass
 
 
@@ -57,3 +59,25 @@ class EditListViewTests(BaseListTestClass):
         expected_url = '/dashboard/'
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, expected_url)
+
+    def test_edit_from_request_that_is_not_owner__expect_403(self):
+        self.client.logout()
+
+        user2_data = {
+            'username': 'brumbroombrim',
+            'email': 'brumbroombrim@foo.bar',
+            'password': 'hellobye123',
+        }
+
+        self.create_user(**user2_data)
+
+        user_2_login_credentials = {
+            'username': 'brumbroombrim',
+            'password': 'hellobye123'
+        }
+
+        self.client.login(**user_2_login_credentials)
+
+        response = self.client.post(reverse(self.PATH, kwargs={'str': self.profile.slug, 'slug': self.user_list.slug}))
+
+        self.assertEqual(response.status_code, 403)
