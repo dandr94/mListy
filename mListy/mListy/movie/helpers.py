4753bc80a041ddeb51024266704268bd9bb2ff0e
dandr94 <d.andreev1994@gmail.com>
2022-12-02 17:36:21 +0200

    Moved some funcs to account helpers.py

--- mListy/mListy/movie/helpers.py
+++ mListy/mListy/movie/helpers.py
@@ -80,14 +80,3 @@ def return_youtube_trailer(movie_name: str, release_date: str) -> str:
     except KeyError:
         return ''
 
-
-def return_last_added_entries(entries: dict) -> List[Tuple[object, int]]:
-    last_added = sorted(entries.items(), key=lambda x: x[0].date_created, reverse=True)[:5]
-
-    return last_added
-
-
-def return_total_average_grade(entries: dict) -> int:
-    total_average_grade = sum(entries.values()) // len(entries) if entries else 0
-
-    return total_average_grade
