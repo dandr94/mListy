39349ab1e5dd9c0a80c23e591b06736a98f4f863
dandr94 <d.andreev1994@gmail.com>
2022-12-06 18:41:58 +0200

    return_time_stats now returns minutes too

--- mListy/mListy/list/helpers.py
+++ mListy/mListy/list/helpers.py
@@ -1,12 +1,13 @@
 from typing import Tuple
 
 
-def return_time_stats(total_minutes: int) -> Tuple[float, float]:
-    days = total_minutes / 1440
+def return_time_stats(total_minutes: int) -> Tuple[float, ...]:
+    days = total_minutes // 1440
     left_over_minutes = total_minutes % 1440
-    minutes = left_over_minutes / 60
+    hours = left_over_minutes // 60
+    minutes = total_minutes % 60
 
-    return days, minutes
+    return days, hours, minutes
 
 
 def return_list_average_grade(entries: list) -> int:
