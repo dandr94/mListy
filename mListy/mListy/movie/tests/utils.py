5467799a87c99d9824d7abe32f15e4ad3a913827
dandr94 <d.andreev1994@gmail.com>
2023-02-18 17:31:32 +0200

    Add additional entry

--- mListy/mListy/movie/tests/utils.py
+++ mListy/mListy/movie/tests/utils.py
@@ -33,3 +33,21 @@ VALID_MOVIEDB_DATA_2 = {
     'release_date': '2022-12-08',
     'status': 'Released'
 }
+
+VALID_MOVIEDB_DATA_3 = {
+    'movie_id': 555,
+    'name': 'leetlizard 2',
+    'poster': 'https://foo.bar',
+    'description': 'Lorem ipsum un mass foo bar.',
+    'duration': 155,
+    'genres': 'Action, Drama',
+    'average_grade': 9.5,
+    'actors': 'Foo, Bar, Barz',
+    'roles': 'Barz, Bar, Foo',
+    'production_companies': 'FromBarzSoftware',
+    'language': 'En',
+    'imdb_link': 'https://foo.bar',
+    'budget': 1500000.0,
+    'release_date': '2022-12-08',
+    'status': 'Released'
+}
