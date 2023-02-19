fd0210c12bdf5173235e658b54891286f63848f1
dandr94 <d.andreev1994@gmail.com>
2023-02-19 20:45:32 +0200

    Add return_trending_movies func

--- mListy/mListy/account/helpers.py
+++ mListy/mListy/account/helpers.py
@@ -1,4 +1,6 @@
 from typing import List
+import tmdbsimple as tmdb
+from mListy.movie.helpers import MovieBlueprint
 
 
 def return_last_added_entries(entries: list) -> List[object]:
@@ -11,3 +13,14 @@ def return_total_average_grade(entries: list) -> int:
     total_average_grade = sum(e.grade for e in entries) / len(entries) if entries else 0
 
     return total_average_grade
+
+
+def return_trending_movies() -> List[MovieBlueprint]:
+    trending = tmdb.Trending(media_type='movie', time_window='week')
+
+    res = []
+
+    for movie in trending.info()['results'][0:12]:
+        res.append(MovieBlueprint(movie['id'], movie['title'], movie['poster_path'], movie['vote_average']))
+
+    return res
