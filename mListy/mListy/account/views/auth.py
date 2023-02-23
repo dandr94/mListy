624c0ecda1e36b4d995205066474ccdf15599c55
dandr94 <d.andreev1994@gmail.com>
2023-02-23 20:23:21 +0200

    Fix next to properly redirect

--- mListy/mListy/account/views/auth.py
+++ mListy/mListy/account/views/auth.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -22,7 +23,12 @@ class LoginUserView(LoginView):
     template_name = 'account/login.html'
 
     def get_success_url(self):
-        return reverse_lazy('dashboard')
+        next_url = self.request.POST.get('next', '')
+
+        if next_url and url_has_allowed_host_and_scheme(next_url, None):
+            return next_url
+        else:
+            return super().get_success_url()
 
 
 class LogoutUserView(LogoutView):
