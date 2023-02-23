57559c8278c131bb1b2086d29bc2993338fc8264
dandr94 <d.andreev1994@gmail.com>
2023-02-23 22:46:13 +0200

    Add lists_collapse.js

---
+++ mListy/static/js/lists_collapse.js
@@ -0,0 +1,23 @@
+//https://codepen.io/mrvinrsk/pen/GRBaOGJ
+
+if (document.querySelector(".fixed")) {
+  document.querySelector(".fixed .handle").addEventListener("click", () => {
+    document.querySelector(".fixed").classList.toggle("hidden");
+
+    if (!document.querySelector(".fixed").classList.contains("hidden")) {
+      if(document.querySelector(".fixed").hasAttribute("data-auto-close")) {
+        let duration = document.querySelector(".fixed").dataset.autoClose || 10000;
+        if(duration < 1000) duration = 1000;
+
+        setTimeout(() => {
+          document.querySelector(".fixed").classList.add("hidden");
+        }, duration);
+      }
+    }
+  });
+
+  setTimeout(() => {
+    document.querySelector(".fixed").classList.add("hidden");
+    document.querySelector(".fixed").classList.remove("first");
+  }, 1500);
+}
