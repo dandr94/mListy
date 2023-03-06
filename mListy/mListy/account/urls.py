68d32e7f7cd5acee6732d75ba331e9ef7066ec21
dandr94 <d.andreev1994@gmail.com>
2023-03-06 15:23:45 +0200

    Added urls for password_reset

--- mListy/mListy/account/urls.py
+++ mListy/mListy/account/urls.py
@@ -1,18 +1,28 @@
 from django.urls import path
-
-from mListy.account.views.auth import RegisterUserView, LoginUserView, LogoutUserView
+from mListy.account.views.auth import RegisterUserView, LoginUserView, LogoutUserView, ResetPasswordView, \
+    ResetPasswordCompleteView, ResetPasswordConfirmView, ResetPasswordDoneView
 from mListy.account.views.generic import HomeViewNoProfile, Dashboard, About
 from mListy.account.views.profile import ProfileDetailsView, EditProfileView, ChangePasswordView
 
+
 urlpatterns = [
+    # Generic
     path('', HomeViewNoProfile.as_view(), name='index'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('about/', About.as_view(), name='about'),
+
+    # Auth
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
-    path('change_password/', ChangePasswordView.as_view(), name='change password'),
-    path('about/', About.as_view(), name='about'),
+    path('reset_password/', ResetPasswordView.as_view(), name='reset_password'),
+    path('reset_password_sent/', ResetPasswordDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', ResetPasswordConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', ResetPasswordCompleteView.as_view(), name='password_reset_complete'),
 
+    # Profile
     path('profile/<str:slug>/', ProfileDetailsView.as_view(), name='details profile'),
-    path('profile/edit/<str:slug>/', EditProfileView.as_view(), name='edit profile')
+    path('profile/edit/<str:slug>/', EditProfileView.as_view(), name='edit profile'),
+    path('change_password/', ChangePasswordView.as_view(), name='change password'),
+
 ]
